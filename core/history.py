46ce1fb5711b7b18749d6569e3a3bdcdad9e7bf6
Busbar500kV <yasasponweera@gmail.com>
2025-08-12 18:45:46 -0700

    Create history.py

---
+++ core/history.py
@@ -0,0 +1,10 @@
+# core/history.py
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass
+class History:
+    t: List[int] = field(default_factory=list)
+    E_cell: List[float] = field(default_factory=list)
+    E_env:  List[float] = field(default_factory=list)
+    E_flux: List[float] = field(default_factory=list)
